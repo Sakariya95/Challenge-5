b73d5269959078014d9fc27697adea3a46be33f6
Sakariya95 <niyyex@live.co.uk>
2022-12-08 23:56:30 +0000

    done

--- workdayscheduler/README.md
+++ workdayscheduler/README.md
@@ -1,2 +1,4 @@
 # Challenge-5
+
 Day Scheduler
+https://sakariya95.github.io/Challenge-5/workdayscheduler/index.html
--- workdayscheduler/script.js
+++ workdayscheduler/script.js
@@ -1,37 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+const clock = document.getElementById("clock");
+setInterval(() => {
+    const now = moment();
+    const readable = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
+    clock.textContent = readable;
 
-$(function toDate(currentDay) {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  const parts = {
-    date: currentDay.getDate(),
-    month: currentDay.getMonth() + 1,
-    year: currentDay.getFullYear(),
-    hour: (currentDay.getHours() % 12) || 12,
-    minute: currentDay.getMinutes().toString().padStart(2, "0"),
-    amOrPm: currentDay.getHours() < 12 ? "AM" : "PM"
-  };
-  return "{parts.date}/{parts.month}/{parts.year} {parts.hour}:{parts.minute} {parts.amOrPm}";
-})
-const myDate = new Date();
-const myNewDate = toDate(myDate);
+}, 1000);
 
-console.log(myNewDate);
+
+    var saveBtn = document.querySelectorAll(".saveBtn");
+    
+    saveBtn.forEach(function(node) {
+       
+        node.addEventListener("click", function() {
+           
+            var description = $(this).siblings(".description").val();
+            
+            var time = $(this).parent().attr("id");
+            
+            localStorage.setItem(time, description);
+    
+        })
+    });
+    
+    
+    $("#hour1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour5 .description").val(localStorage.getItem("hour-5"));
+    $("#hour6 .description").val(localStorage.getItem("hour-6"));
+    $("#hour7 .description").val(localStorage.getItem("hour-7"));
+    $("#hour8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour10 .description").val(localStorage.getItem("hour-10"));
+    
+
+    var elements = $(".time-block");
+    var d = new Date();
+    var h = d.getHours();
+
+
+    for (i = 0; i < elements.length; i++) {
+        var t = parseInt(elements[i].getAttribute("data-hour"));
+
+        if (h < t) {
+            elements[i].classList.add('future');
+            elements[i].classList.remove('past');
+            elements[i].classList.remove('present');
+        } else if (h > t) {
+            elements[i].classList.add('past');
+            elements[i].classList.remove('future');
+            elements[i].classList.remove('present');
+        } else if (h === t) {
+            elements[i].classList.add('present');
+            elements[i].classList.remove('future');
+            elements[i].classList.remove('past');
+        } else if (h > 16 && h < 9) {
+            elements[i].classList.add('future');
+            elements[i].classList.remove('past');
+            elements[i].classList.remove('present');
+        }
+    }
